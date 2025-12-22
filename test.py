2d35c9182632c1511e21675844f3df8b69d8732e
Iseyg91 <gokalpahmet74@gmail.com>
2025-12-22 19:10:47 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,95 @@
+    # ===== RÔLES QUI COMPTENT POUR LES SLOTS =====
+    SLOT_COUNT_ROLES = {
+
+        # Les 8 Gifted Events → 6ceb87
+        1452006766404567051: ("Gifted Windy Bee", "<:WindyBee_Gifted:1452044744090976388>"),
+        1452010048938311964: ("Gifted Vicious Bee", "<:ViciousBee_Gifted:1452045941904375880>"),
+        1452010023839596685: ("Gifted Tabby Bee","<:TabbyBee_Gifted:1452046096212820028>"),
+        1452010053342331001: ("Gifted Photon Bee","<:PhotonBee_Gifted:1452046240161337526>"),
+        1452010058425700674: ("Gifted Festive Bee","<:FestiveBee_GiftedFixed:1452046361909399705>"),
+        1452023089822171336: ("Gifted Gummy Bee","<:GummyBee_Gifted:1452046503618023576>"),
+        1452303648821608601: ("Gifted Bear Bee","<:BearBee_Gifted:1452364890105970913>"),
+        1452331611780808704: ("Gifted Digital Bee","<:DigitalBee_Gifted:1452365122000392283>"),
+        
+        # Les Gifted MY → ff00ff
+        1451863953213882439: ("Gifted Buoyant Bee", "<:Buoyant_Bee_Gifted:1452019065194287187>"),
+        1451864071866417192: ("Gifted Fuzzy Bee", "<:FuzzyBee_Gifted:1452018891839770634>"),
+        1451864139525001226: ("Gifted Precise Bee","<:PreciseBee_Gifted:1452018727519391794>"),
+        1451864193589579868: ("Gifted Spicy Bee","<:SpicyBee_Gifted:1452018510845841571>"),
+        1451864236845301821: ("Gifted Tadpole Bee","<:TadpoleBee_Gifted:1452018230498693212>"),
+        1451864269468467386: ("Gifted Vector Bee","<:GiftedVectorBee:1452017917003698328>"),
+
+        # Les Gifted Leg → ff00ff
+        1451866792296448014: ("Gifted Diamond Bee","<:DiamondBee_Gifted:1452022674610983053>"),
+        1451866855005487105: ("Gifted Ninja Bee","<:NinjaBee_Gifted:1452022441160216808>"),
+        1451866868552962089: ("Gifted Shy Bee","<:ShyBee_Gifted:1452022231214592011>"),
+        1451866881278480536: ("Gifted Demon Bee","<:DemonBee_Gifted:1452022055787696200>"),
+        1451866891369975849: ("Gifted Baby Bee","<:BabyBee_Gifted:1452021873822142475>"),
+        1451866901335507029: ("Gifted Music Bee","<:MusicBee_Gifted:1452021666627588167>"),
+
+        # Les Gifted Epic → ff00ff
+        1452006029880459436: ("Gifted Fire Bee","<:FireBee_Gifted:1452025038680100964>"),
+        1452006741934866512: ("Gifted Riley Bee", "<:RileyBee_Gifted:1452025741347651861>"),
+        1452006016471404625: ("Gifted Bucko Bee","<:BuckoBee_Gifted:1452026051877273612>"),
+        1452006019310944266: ("Gifted Bubble Bee","<:BubbleBee_Gifted:1452026219762810910>"),
+        1452006030614728774: ("Gifted Honey Bee","<:HoneyBee_Gifted:1452026356815630417>"),
+        1452006033508667504: ("Gifted Exhausted Bee","<:ExhaustedBee_Gifted:1452026553901777098>"),
+
+        # Les Gifted Rare → ff00ff
+        1452006030807531531: ("Gifted Cool Bee","<:CoolBee_Gifted:1452029027098562610>"),
+        1452006034208985240: ("Gifted Bumble Bee","<:BumbleBee_Gifted:1452029180924399828>"),
+        1452006035077337129: ("Gifted Rad Bee","<:RadBee_Gifted:1452029436324216935>"),
+        1452006037208170546: ("Gifted Rascal Bee","<:RascalBee_Gifted:1452029580662538503>"),
+        1452006764957536448: ("Gifted Looker Bee","<:LookerBee_Gifted:1452030052844699791>"),
+        1452006765796528200: ("Gifted Bomber Bee","<:BomberBee_Gifted:1452030300983918675>"),
+        
+        1451989002012983417: ("Gifted Basic Bee","<:BasicBee_Gifted:1452000782860943420>"),
+        
+        # Les 8 Events → 6ceb87
+        1452010055812513814: ("Windy Bee", "<:Windy_Bee:1452047618224947392>"),
+        1452010037605040259: ("Vicious Bee", "<:Vicious_Bee:1452047747967357051>"),
+        1452010051647832255: ("Tabby Bee","<:Tabby_Bee:1452047875243508036>"),
+        1452010039173971968: ("Photon Bee","<:Photon_Bee:1452048028243591249>"),
+        1452010041694486528: ("Festive Bee","<:Festive_BeeFixed:1452048149135757553>"),
+        1452023087213187232: ("Gummy Bee","<:Gummy_Bee:1452048341780136016>"),
+        1452023087213187232: ("Bear Bee","<<:Bear_Bee:1452365423541747825>"),
+        1452331636086800384: ("Digital Bee","<:Digital_Bee:1452365415991742504>"),
+        
+        # Les 6 Mythic → a76cff
+        1452006009110528182: ("Buoyant Bee","<:Buoyant_Bee:1451932706756628542>"),
+        1452006763845910569: ("Fuzzy Bee","<:Fuzzy_Bee:1451932530424025249>"),
+        1452006027842158612: ("Precise Bee","<:Precise_Bee:1451932157890269415>"),
+        1452006028098015375: ("Spicy Bee","<:Spicy_Bee:1451932382327341056>"),
+        1452006023589134561: ("Tadpole Bee", "<:Tadpole_Bee:1451932032061014149>"),
+        1452006021462491167: ("Vector Bee","<:Vector_Bee:1451931920672755845>"),
+        
+        # Les 6 Leg → a76cff
+        1452006031738540185: ("Diamond Bee","<:Diamond_Bee:1451931741014196466>"),
+        1452006029025083472: ("Ninja Bee","<:Ninja_Bee:1451931603751276544>"),
+        1452006025652601042: ("Shy Bee","<:Shy_Bee:1451931464395653332>"),
+        1452006033261203578: ("Demon Bee","<:Demon_Bee:1451931314117673040>"),
+        1452006032170815590: ("Baby Bee","<:Baby_Bee:1451931193061933177>"),
+        1452006027376726026: ("Music Bee","<:Music_Bee:1451931031719645205>"),
+        
+        # Les 6 Epic → a76cff
+        1451866658644951080: ("Fire Bee","<:Fire_Bee:1451930880921833623>"),
+        1451866717247639602: ("Ryley Bee","<:Riley_Bee:1451929168609673256>"),
+        1451866732875485214: ("Bucko Bee","<:Bucko_Bee:1451928879513079879>"),
+        1451866754849706106: ("Bubble Bee","<:Bubble_Bee:1451928717914804306>"),
+        1451866768548036702: ("Honey Bee","<:Honey_Bee:1452010061877608701>"),
+        1451866779281395783: ("Exhausted Bee","<:Exhausted_Bee:1451928425215561779>"),
+        
+            # Les 6 Rare → a76cff
+        1451866428436119572: ("Cool Bee","<:Cool_Bee:1451928204309696716>"),
+        1451866491594211421: ("Bumble Bee","<:Bumble_Bee:1451927944955039867>"),
+        1451866528810008637: ("Rad Bee","<:Rad_Bee:1451927353495388332>"),
+        1451866602495803393: ("Rascal Bee","<:Rascal_Bee:1451926859037020292>"),
+        1451866624800985180: ("Looker Bee","<:Looker_Bee:1451926485370929354>"),
+        1451866638524878848: ("Bomber Bee","<:Bomber_Bee:1451926104217747528>"),
+        
+        # Le Basic → a76cff
+        1451866393447501874: ("Basic Bee","<:Basic_Bee:1451925416410484746>"),
+
+        # Xalte → 3d2929
+        1452267115385065532: ("Xalte Bee","<:Xalte_Bee:1452267993022070784>"),
+    }
